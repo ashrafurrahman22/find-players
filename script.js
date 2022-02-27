b31bf5a081efab997c1af3766f1f396fd121364f
ashrafurrahman22 <ashrafurrahman22@gmail.com>
2022-02-27 10:27:56 +0600

    first

--- script.js
+++ script.js
@@ -2,24 +2,35 @@ const playersDiv = document.getElementById('players-div');
 const playerDetails = document.getElementById('player-details');
 const newDiv2 = document.createElement('div');
 const newDiv = document.createElement('div');
-
+// spinner
+const spinner = document.getElementById('spinner');
 
 const allPlayers = () => {
-    const searchBox = document.getElementById('search-box');
-    const searchBoxValue = searchBox.value;
+
+   const searchBox = document.getElementById('search-box');
+   const searchBoxValue = searchBox.value;
+
    searchBox.value = '';
-   
     
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchBoxValue}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displayPlayers(data.player))
+    .then(data => {
+        if (data.player == null) {
+            spinner.style.display = 'block';
+        }
+        else {
+            displayPlayers(data.player);
+            spinner.style.display = 'none'
+        }
+
+    } )
 }
         newDiv.textContent = '';
 
 const displayPlayers = (players) => {
     for(const player of players) {
-       
+
         newDiv.innerHTML = `
         <img class="w-50 rounded-3" src="${player.strThumb}" alt=""> 
         <h3>Name: ${player.strPlayer} </h3>
@@ -29,10 +40,12 @@ const displayPlayers = (players) => {
         `;
         
         playersDiv.appendChild(newDiv);
+       }
+        
         
 
     }
-}
+
 
 const loadDetails = (id) => {
     const url2  = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}
